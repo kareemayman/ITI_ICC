a89f787e7e868a00ebeb42bcc165398ea0ce4e2b
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2025-04-05 16:43:35 +0200

    fix(AdvJs Day2 Numerical Seq): Add Type Validation When Appending Or Prepending New Value To The List

--- Adv-JS/Day2/Tasks/solution/sequence.js
+++ Adv-JS/Day2/Tasks/solution/sequence.js
@@ -1,76 +1,84 @@
 function Sequence(s, e, stp) {
-    var list = genList(s, e, stp);
-    this.start = s;
-    this.end = list[list.length - 1];
-    this.step = stp;
+    var list = genList(s, e, stp)
+    this.start = s
+    this.end = list[list.length - 1]
+    this.step = stp
     this.getList = function() {
-        return list;
+        return list
     }
     this.append = function(val) {
+        if (typeof val !== 'number') {
+            let err = new Error('Value must be a number')
+            throw err
+        }
         if (list.length === 0) {
-            this.start = val;
-            this.end = val;
-            list.push(val);
+            this.start = val
+            this.end = val
+            list.push(val)
             return
         }
         if (this.step + this.end === val) {
-            this.end = val;
-            list.push(val);
+            this.end = val
+            list.push(val)
         } else {
-            let err = new Error('Invalid value');
-            throw err;
+            let err = new Error('Invalid value')
+            throw err
         }
     }
     this.prepend = function(val) {
+        if (typeof val !== 'number') {
+            let err = new Error('Value must be a number')
+            throw err
+        }
         if (list.length === 0) {
-            this.start = val;
-            this.end = val;
-            list.push(val);
+            this.start = val
+            this.end = val
+            list.push(val)
             return
         }
         if (this.start - this.step === val) {
-            this.start = val;
-            list.unshift(val);
+            this.start = val
+            list.unshift(val)
         } else {
-            let err = new Error('Invalid value');
-            throw err;
+            let err = new Error('Invalid value')
+            throw err
         }
     }
     this.pop = function() {
         if (list.length > 0) {
             this.end = list[list.length - 2]
-            return list.pop();
+            return list.pop()
         } else {
-            let err = new Error('List is empty');
+            let err = new Error('List is empty')
             throw err;
         }
     }
     this.deque = function() {
         if (list.length > 0) {
             this.start = list[1] 
-            return list.shift();
+            return list.shift()
         } else {
-            let err = new Error('List is empty');
-            throw err;
+            let err = new Error('List is empty')
+            throw err
         }
     }
 }
 
 function genList(s, e, stp) {
-    var list = [];
+    var list = []
     for (var i = s; i <= e; i += stp) {
-        list.push(i);
+        list.push(i)
     }
-    return list;
+    return list
 }
 
-let seq = new Sequence(1, 10, 2);
-console.log(seq.getList());
-// seq.append(11);
-// seq.append(12);
+let seq = new Sequence(1, 10, 2)
+console.log(seq.getList())
+// seq.append(11)
+// seq.append(12)
 // seq.prepend(0)
 // seq.prepend(-1)
-seq.pop();
+seq.pop()
 // seq.deque()
 // seq.prepend(1)
-console.log(seq.getList());
+console.log(seq.getList())
