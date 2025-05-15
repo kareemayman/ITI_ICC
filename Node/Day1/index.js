edd1f76cb556fcc5c818ab33c36953f02974d0a4
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2025-05-15 17:09:12 +0300

    feat(node bonus task): add CRUD operations on the server

--- Node/Day1/index.js
+++ Node/Day1/index.js
@@ -1,8 +1,26 @@
 const http = require("http")
+const bookServices = require("./bookServices.js")
 
 const server = http.createServer((req, res) => {
 	res.writeHead(200)
-	console.log("request recieved")
+	if ((req.url === "/books" || req.url === "/") && req.method === "GET") {
+		console.log("books request")
+		bookServices.getAllBooks();
+	} else if (/^\/books\/([0-9])+$/.test(req.url) && req.method === "GET") {
+		const id = req.url.split("/")[2]
+		bookServices.getBook(+id)
+	} else if (/^\/books\/([0-9])+$/.test(req.url) && req.method === "DELETE") {
+		const id = req.url.split("/")[2]
+		bookServices.deleteBook(+id);
+	} else if (/^\/books\/([0-9])+\/([a-zA-Z0-9])+$/.test(req.url) && req.method === "PUT") {
+		const id = req.url.split("/")[2]
+		const name = req.url.split("/")[3]
+		bookServices.updateBook(+id, name);
+	} else if (/^\/books\/([0-9])+\/([a-zA-Z0-9])+$/.test(req.url) && req.method === "POST") {
+		const id = req.url.split("/")[2]
+		const name = req.url.split("/")[3]
+		bookServices.addBook({"id": +id, "title": name});
+	}
 	res.end()
 })
 
